03bad33d8e1590a8c594c25b385a7951b36f6b11
Jason Vanstone <jason@chameleoncreative.com>
2021-11-10 17:30:22 -0500

    added confirm email to woo

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ add_theme_support( 'editor-style' );
  * Registers an editor stylesheet in a sub-directory.
  */
 function add_editor_styles_sub_dir() {
-    add_editor_style( trailingslashit( get_template_directory_uri() ) . 'editor-style.css' );
+	add_editor_style( trailingslashit( get_template_directory_uri() ) . 'editor-style.css' );
 }
 add_action( 'after_setup_theme', 'add_editor_styles_sub_dir' );
 
@@ -60,3 +60,46 @@ function add_footer_info() {
 <?php
 }
 add_action( 'astra_content_bottom', 'add_footer_info' );
+
+
+/**
+* @snippet       Add "Confirm Email Address" Field @ WooCommerce Checkout
+* @how-to        Get CustomizeWoo.com FREE
+* @author        Rodolfo Melogli
+* @testedwith    WooCommerce 3.8
+* @donate $9     https://businessbloomer.com/bloomer-armada/
+*/
+  
+// ---------------------------------
+// 1) Make original email field half width
+// 2) Add new confirm email field
+  
+add_filter( 'woocommerce_checkout_fields' , 'bbloomer_add_email_verification_field_checkout' );
+   
+function bbloomer_add_email_verification_field_checkout( $fields ) {
+  
+$fields['billing']['billing_email']['class'] = array( 'form-row-first' );
+  
+$fields['billing']['billing_em_ver'] = array(
+	'label' => 'Confirm Email',
+	'required' => true,
+	'class' => array( 'form-row-last' ),
+	'clear' => true,
+	'priority' => 999,
+);
+  
+return $fields;
+}
+  
+// ---------------------------------
+// 3) Generate error message if field values are different
+  
+add_action('woocommerce_checkout_process', 'bbloomer_matching_email_addresses');
+  
+function bbloomer_matching_email_addresses() { 
+	$email1 = $_POST['billing_email'];
+	$email2 = $_POST['billing_em_ver'];
+	if ( $email2 !== $email1 ) {
+		wc_add_notice( 'Your email addresses do not match', 'error' );
+	}
+}
