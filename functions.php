ec110825cf1fea61152abe4f713caae3f010266b
Jason Vanstone <jason@chameleoncreative.com>
2021-12-09 14:03:00 -0500

    showing additional information

--- functions.php
+++ functions.php
@@ -411,7 +411,7 @@ function woo_remove_product_tabs( $tabs ) {
 
     unset( $tabs['description'] );      	// Remove the description tab
     unset( $tabs['reviews'] ); 			// Remove the reviews tab
-    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+   // unset( $tabs['additional_information'] );  	// Remove the additional information tab
 
     return $tabs;
 
